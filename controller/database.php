a02a11d1bec72e2aebf8f9440bf89eef0e5ddbb1
nqduy65 <nguyenquocduy06051998@gmail.com>
2022-12-06 22:36:10 +0700

    update conncection db name 'data' -> 'name' & update folder phpmailer

--- controller/database.php
+++ controller/database.php
@@ -9,6 +9,6 @@ $conn = mysqli_connect($hostname, $username, $password)
 // echo "Connected to MySQL<br>";
 
 //select a database to work with
-$selected = mysqli_select_db($conn, "data")
+$selected = mysqli_select_db($conn, "btl")
 	or die("Could not select data");
 ?>
